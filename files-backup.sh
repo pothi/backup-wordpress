9466529af848d96a201d44ed04ae365841080011
Pothi Kalimuthu <pothi@tinywp.com>
2015-03-29 22:50:16 +0530

    Keep only 4 latest backups and remove other older backups

--- files-backup.sh
+++ files-backup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 # Takes backup of all the sites
+# Keeps 4 latest backups
+# Removes oldest backups (older than 4 backups)
+
+### Files are named in the following way...
+# all-files-xyz-1-date.tar.gz (latest backup)
+# all-files-xyz-2-date.tar.gz (older backup)
+# all-files-xyz-3-date.tar.gz (older backup)
+# all-files-xyz-4-date.tar.gz (oldest backup)
 
 ### Variables
 
@@ -19,7 +27,17 @@ EXC_PATH_3=$INC_PATH/wp-content/uploads/backups
 ### Do not edit below this line ###
 
 # For all sites
-BACKUP_FILE_NAME=${BACKUP_PATH}all-files-$(hostname -f | awk -F $(hostname). '{print $2}')-$(date +%F_%H-%M-%S).tar.gz
+BACKUP_FILE_NAME=${BACKUP_PATH}all-files-$(hostname -f | awk -F $(hostname). '{print $2}')
+
+# Remove the oldeest file
+rm ${BACKUP_FILE_NAME}-4-* &> /dev/null
+
+# Rename other files to make them older
+rename -- -3- -4- ${BACKUP_FILE_NAME}-3-* &> /dev/null
+rename -- -2- -3- ${BACKUP_FILE_NAME}-2-* &> /dev/null
+rename -- -1- -2- ${BACKUP_FILE_NAME}-1-* &> /dev/null
 
 # let's do it using tar
-tar czf $BACKUP_FILE_NAME --exclude=$EXC_PATH_1 --exclude=$EXC_PATH_2 --exclude=$EXC_PATH_3 $INC_PATH &> /dev/null
+# Create a fresh backup
+tar czf ${BACKUP_FILE_NAME}-1-$(date +%F_%H-%M-%S).tar.gz --exclude=$EXC_PATH_1 --exclude=$EXC_PATH_2 --exclude=$EXC_PATH_3 $INC_PATH &> /dev/null
+
